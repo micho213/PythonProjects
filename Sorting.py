70b700c9fbdf5b556978cbf210680a3abf4bba6f
micho213 <117308883@umail.ucc.ie>
2018-02-03 12:59:29 +0000

    assignment 9

---
+++ Sorting.py
@@ -0,0 +1,44 @@
+def Print(data):
+    print("Name    Age")
+    print("------- ---")
+    [print("%-7s %3s" % (name, age)) for name, age in data]
+
+
+def bubbleSort (data, field):
+    for iteration in range(len(data)-1):
+        for index in range(len(data)-1):
+            if field == "age":
+                if (data[index])[1] > (data[index+1])[1]:
+                    change = ((data[index])[0],(data[index])[1])
+                    data[index] = ((data[index +1])[0],(data[index+1])[1])
+                    data[index + 1] =  change
+            if field == "name":
+                if (data[index])[0] > (data[index+1])[0]:
+                    change = ((data[index])[0],(data[index])[1])
+                    data[index] = ((data[index +1])[0],(data[index+1])[1])
+                    data[index + 1] =  change
+    return Print(data)
+
+def SelectionSort(data,field):
+    x = -1
+    for iteration in range(len(data)):
+        for iteration2 in range(len(data)):
+            if field == "age":
+                if (data[iteration])[1] > (data[iteration2])[1]:
+                    largest = ((data[iteration])[0],(data[iteration])[1])
+                    y = iteration
+    data[x] = largest
+    data[y] = ((data[x])[0], (data[x])[1])
+
+    print(data)
+
+"""data[iteration] = ((data[x])[0], (data[x])[1])
+            data[x] = largest
+            x -= 1"""
+
+
+listOfNames = [ ("Ann",23),("Tim",19),("Bob",37),("Ned",51),("Sue",18)]
+print(listOfNames[-1])
+#Print(listOfNames)
+#bubbleSort(listOfNames,"age")
+SelectionSort(listOfNames,"age")
--- multiplesOf3And5.py
+++ multiplesOf3And5.py
@@ -1,8 +1,8 @@
 i = [ ]
 x = 0
-three = 0
+answer = 0
 while x != 999:
     x += 1
     if x%3 == 0 or x%5 == 0:
-        three += x
-        print(three)
+        answer += x
+print(answer)
